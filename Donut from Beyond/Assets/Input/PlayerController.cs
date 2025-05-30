30cdc36958be974952e68ad87f00e608f46e7460
Grecia Lorena Morales Castillo <cdmx2510@amerike.edu.mx>
2025-05-30 11:41:01 -0600

    Cinemachine
    Agregando Cinemachine y corrigiendo errores de movimiento y mira.

--- Donut from Beyond/Assets/Input/PlayerController.cs
+++ Donut from Beyond/Assets/Input/PlayerController.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Configuracion de movimiento")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = 10f;
     [SerializeField] private float hoverHeight = 1.5f;
 
     private IPlayerMovement _movement; // Referencia al movimiento
     private InputReader _inputReader; // Lee losinputs del jugador
+    private CharacterController _controller; // Remplaza al rigidbody para detectar las colisiones
+
+    private void Start()
+    {
+        // Oculta y bloquea el cursor pero en todo momento, falta desbloquearlo en menus xd
+        /*Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;*/
+    }
 
     private void Awake()
     {
+        //Asigna el character controller
+        _controller = GetComponent<CharacterController>();
+
         _inputReader = new InputReader(); //crea el lector de inputs
-        _movement = new PlayerMovement(transform, moveSpeed, rotateSpeed, hoverHeight); 
+
+        _movement = new PlayerMovement(transform, _controller, moveSpeed, rotateSpeed, hoverHeight);
     }
 
     private void Update()
